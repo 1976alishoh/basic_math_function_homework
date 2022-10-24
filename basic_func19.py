5c002ffb562289ce543e980de7f3286469a010fa
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-24 10:01:08 +0300

    19  done

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,7 @@
+def main(a,b):
+    result = b-a
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+    return result
+print(main(4,11))
