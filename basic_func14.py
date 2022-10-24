97a1f2e1e6beed8ecd80999a76eace78717a743f
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-24 09:37:47 +0300

    14  done

--- basic_func14.py
+++ basic_func14.py
@@ -1,3 +1,7 @@
 # Create a function called main.
+def main(a,b):
+    result = a//b
 # Create function arguments a and b.
 # Returns the floor division of a and b.
+    return result
+print(main(11,2))
