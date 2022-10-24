43f64ba5fc75d06fd2d140cae3027b560b507c77
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-24 10:05:27 +0300

    20  done

--- basic_func20.py
+++ basic_func20.py
@@ -1,3 +1,7 @@
+def main(a,b):
+    result = a*b
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
+    return result
+print(main(5,4))
